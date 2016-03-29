6ddc6f7ce8f28c06e0769feccb9ead1e38303bf1
leafaj1290 <jun.baseball1029@gmail.com>
2016-03-29 16:44:13 +0900

    add in students.sql that is sample

---
+++ students.sql
@@ -0,0 +1,27 @@
+create database test;
+create table students(
+student_id char(6),
+student_name varchar(30),
+age int,
+department char(3));
+
+insert into students value ("TB0001","Kato Masaya",25,"Tch");
+insert into students value ("TB0002","Takizawa Hanako",22,"Sci");
+insert into students value ("TB0003","Matsuzaki Hanako",27,"Edu");
+insert into students value ("TB0004","Takizawa Hanako",26,"Edu");
+insert into students value ("TB0005","Matsuzaki Taro",26,"Ecn");
+insert into students value ("TB0006","Saito Shota",29,"Lit");
+insert into students value ("TB0007","Saito Shota",23,"Agl");
+insert into students value ("TB0008","Kato Hanako",22,"Edu");
+insert into students value ("TB0009","Sato Taro",27,"Edu");
+insert into students value ("TB0010","Kato Masaya",22,"Phm");
+insert into students value ("TB0011","Matsuzaki Ken",29,"Tch");
+insert into students value ("TB0012","Takizawa Hanako",24,"Edu");
+insert into students value ("TB0013","Tanaka Taro",26,"Med");
+insert into students value ("TB0014","Kato Shota",23,"Agl");
+insert into students value ("TB0015","Takizawa Shota",21,"Phm");
+insert into students value ("TB0016","Matsuzaki Ken",27,"Edu");
+insert into students value ("TB0017","Tanaka Shun",29,"Ecn");
+insert into students value ("TB0018","Sendai Shun",25,"Tch");
+insert into students value ("TB0019","Sato Ken",27,"Med");
+insert into students value ("TB0020","Tanaka Shota",20,"Lit");
